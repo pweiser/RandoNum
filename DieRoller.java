7e050e4a305afc8fa08dca77c479fd7435f38396
Phillip Weiser <phillipweiser27@gmail.com>
2019-02-25 14:22:05 -0500

    Adding filewrite

--- DieRoller.java
+++ DieRoller.java
@@ -1,11 +1,19 @@
-import Die;
+import Die.java;
+import java.io.File;
 
 class DieRoller
 {
     public static void main(String [] args)
     {
         Die myDie = new Die();
-        myDie.rollDie();
-        myDie.writeFile(args[0]);
+        int result = myDie.rollDie();
+        myDie.writeFile(args[0], result);
+    }
+
+    public static void writeFile(String file, int res){
+	BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+	writer.write(str);
+
+	writer.close();
     }
 }
