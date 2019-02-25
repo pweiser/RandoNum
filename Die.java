5eccbf946889759aa8c96954ddccaa160ed0a0a5
Mario Castellanos <maariocastellanos@gmail.com>
2019-02-25 14:16:15 -0500

    DieClass

---
+++ Die.java
@@ -0,0 +1,15 @@
+import java.util.Random 
+
+
+class Die (){
+	public static void main(String[] args){
+		public Die(){
+			Random rando = new Random(); 
+		}
+
+		public rollDie(){
+			return rando.nextInt(6); 
+		}
+
+	}
+}
