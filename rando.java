f3bfbf83711c81575b939904a1af4f211d162349
Phillip Weiser <phillipweiser27@gmail.com>
2019-02-25 13:55:53 -0500

    print a random number

---
+++ rando.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+class rando{
+    public void main(){
+	Random rand = new Random();
+
+	int n = rand.nextInt();
+
+	system.out.println("Here is a random number: " + n);
+    }
+}
